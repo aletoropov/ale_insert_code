a5b1d0b68850c2a0a72fb7505c6318ec56a54499
Alexandr <toropovsite@yandex.ru>
2023-12-23 07:15:13 +0300

    Start project

---
+++ ale-insert-code.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Ale Imnsert Code
+Description: Плагин для добавления произвольного кода в тему WordPress
+Version: 1.0.0
+Author: Toropov Aleksandr
+Author URI: https://aletoropov.ru/
+ */
+
+ 
