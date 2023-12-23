58d0d777b4c27311305585d79bf99e2e4d4586cf
Alexandr <toropovsite@yandex.ru>
2023-12-23 07:39:44 +0300

    Заглушки функций активации и деактивации плагина

--- ale-insert-code.php
+++ ale-insert-code.php
@@ -7,4 +7,22 @@ Author: Toropov Aleksandr
 Author URI: https://aletoropov.ru/
  */
 
- 
+/**
+ * Действия при активации плагина
+ */
+register_activation_hook( __FILE__, 'ale_install' );
+function ale_install() {
+   // проверяем совместимость с версией WordPress
+   if ( !is_wp_version_compatible( '5.2' )) {
+      wp_die( 'This plugin reqires WordPress 5.2 or higter.' );
+   }
+}
+
+/**
+ * Действия при деактивации плагина
+ */
+register_deactivation_hook( __FILE__, 'ale_deactivation' );
+function ale_deactivation() {
+   // действия при деактивации плагина
+}
+
