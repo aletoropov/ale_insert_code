b50196a47d53ea65b8cbd5e623d9d79cfae81106
Alexandr <toropovsite@yandex.ru>
2024-01-04 17:31:38 +0300

    Очистка опций при удалении плагина

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,14 @@
 <?php
+/**
+ * Код выполняемый при удалении плагина
+ */
+
+// if uninstall.php is not called by WordPress, die
+if (!defined( 'WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+delete_option('aic_body_code');
+delete_option('aic_head_code');
+delete_option('aic_footer_code');
+delete_option('aic_admin_code');
